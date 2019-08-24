7f5ae8ff593f174e3dea81e2766b3adc9e189606
chenpionaar1998 <chenpionaar1998@gmail.com>
2019-08-23 18:58:00 -0700

    fixing port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 const path = require('path');
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public/'));
